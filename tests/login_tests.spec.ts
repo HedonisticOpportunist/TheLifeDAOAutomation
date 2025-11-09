8fd51d07bd329332891765f2b07efcccb4af9a04
HedonisticOpportunist <fujitahibaraki@gmail.com>
2025-11-09 22:17:08 +0000

    Add files via upload

---
+++ tests/login_tests.spec.ts
@@ -0,0 +1,26 @@
+import { expect, test } from "@playwright/test";
+import { login, verifyLoginPage } from "../helpers/testHelpers";
+
+test.describe("LOGIN-RELATED TESTS", () => {
+  test.beforeEach(async ({ page }) => {
+    await verifyLoginPage(page);
+  });
+
+  test("a user cannot login without inputting a valid email", async ({
+    page,
+  }) => {
+    await login(page, "x", "password", true);
+  });
+
+  test("a user cannot login without a valid password and email", async ({
+    page,
+  }) => {
+    // Test passes, but the functionality is not what you would expect in this scenario. View the README.md file.
+    await login(page, "test@gmail.com", "password", false);
+  });
+
+  test("a user with an existing credentials can login", async ({ page }) => {
+    await login(page, "fujitahibaraki.gmail.com", "bengD2bD3zZSwnW!123", true);
+    await expect(page.getByText("Activate Your Membership")).toBeVisible();
+  });
+});
---
+++ tests/sign_up_tests.spec.ts
@@ -0,0 +1,75 @@
+import { test } from "@playwright/test";
+import {
+  enterSignUpCode,
+  signUp,
+  joinWaitingList,
+  verifyUATPage,
+} from "../helpers/testHelpers";
+
+test.describe("SIGN-UP-RELATED TESTS", () => {
+  test.beforeEach(async ({ page }) => {
+    await verifyUATPage(page);
+  });
+
+  test("a user cannot sign up using an invalid invite code", async ({
+    page,
+  }) => {
+    await enterSignUpCode(page, "123", true, false);
+  });
+
+  test("a user cannot sign up without inputting an invite code", async ({
+    page,
+  }) => {
+    await enterSignUpCode(page, "", false, true);
+  });
+
+  test("a user cannot sign up for the waiting list without an email", async ({
+    page,
+  }) => {
+    await joinWaitingList(page, "", true, false, false, true);
+  });
+
+  test("a user cannot sign up for the waiting list with an invalid email", async ({
+    page,
+  }) => {
+    await joinWaitingList(page, "test", false, true, false, true);
+  });
+
+  test("a user cannot sign up for the waiting list if they are already a member", async ({
+    page,
+  }) => {
+    await joinWaitingList(page, "test@email.com", false, false, true, false);
+  });
+
+  test("a user cannot complete the signup without providing a valid email", async ({
+    page,
+  }) => {
+    await enterSignUpCode(page, "AFypw5", false, false);
+    await signUp(page, "test", "password123!", true, false, false);
+  });
+
+  test("a user cannot complete the signup without providing a password with special characters", async ({
+    page,
+  }) => {
+    await enterSignUpCode(page, "AFypw5", false, false);
+    await signUp(page, "test@email.com", "password", false, true, false);
+  });
+
+  test("a user cannot complete the signup without providing a password with special characters or an email", async ({
+    page,
+  }) => {
+    await enterSignUpCode(page, "AFypw5", false, false);
+    await signUp(page, "test", "password", true, true, false);
+  });
+
+  test.skip("a user can complete the signup by providing valid details", async ({
+    page,
+  }) => {
+    /* Running this will invalidate the sign-up code. 
+    // A good solution for automated testing would involve creating a list of throwaway codes
+    that would allow for this to be run multiple times.
+    */
+    await enterSignUpCode(page, "AFypw5", false, false);
+    await signUp(page, "email@email.com", "password123!", false, false, true);
+  });
+});
